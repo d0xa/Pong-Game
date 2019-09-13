a408ab3736187e437f32cbbce5663a855563243a
Jonathan Roman <jonathan.roman519@gmail.com>
2019-09-12 20:55:15 -0600

    created ball class and two contructors for it

---
+++ Ball.py
@@ -0,0 +1,24 @@
+class Ball:
+	def __init__(size,xSpeed,ySpeed):
+		ballSize = size;
+		ballXSpeed = type(float)xSpeed;
+		ballYSpeed = type(float)ySpeed;
+
+	def __init__(size):
+		ballSize = size;
+		ballXSpeed = 1.0;
+		ballYSpeed = 1.0;
+
+# The spawn method will take an int and a boolean, when true the method will
+# randomize speed and direction otherwise the ball(s) will all be realeased with 
+# the same speed and direaction
+
+	def spawn(numOfBalls,RandomSpeedBool):
+		for i in numOfBalls:
+			if(RandomSpeedBool):
+
+			else:
+				
+
+
+
